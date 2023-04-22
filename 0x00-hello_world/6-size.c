39defd7ddf89535d598b9125a0b98b9e8e2a8bfa
Hajjaj05 <hajjaj1047@gmail.com>
2023-04-22 14:58:09 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,5 @@ printf("size of an init: %d byte(s)\n". sizeof(init));
 printf("size of a long init: %d byte(s)\n". sizeof(long init));
 printf("size of a long long init: %d byte(s)\n". sizeof(long long init));
 printf("size of a float: %d byte(s)\n". sizeof(float));
-return (0):
+return (0);
 }
