a6222dc5f1a3fac5a3475bfdc70a0431b28341aa
Hajjaj05 <hajjaj1047@gmail.com>
2023-04-23 07:50:44 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,7 @@
 int main(void)
 {
      int n;
+   
      srand(time(0));
      n = rand() - RAND_MAX / 2;
      if (n > 0)						        printf("%d is positive\n", n);
@@ -18,5 +19,3 @@ int main(void)
            printf("%d is negative\n", n);
      return (0);
 }
-
-
