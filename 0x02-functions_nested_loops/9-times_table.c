b582aa5c047db952cc507350e0418229468403c3
Hajjaj05 <hajjaj1047@gmail.com>
2023-04-26 03:51:44 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
-/***
- * times_table - print the 9 times table
+/**
+ * times_table - print the 9 times table starting with 0
  */
 void times_table(void)
 {
+
 	int num, mult, prod;
 
 	for (num = 0; num <= 9; num++)
