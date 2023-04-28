6f1a4cd2e8e93ff65760e8596652f84a10577ca8
Hajjaj05 <hajjaj1047@gmail.com>
2023-04-28 15:32:14 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,26 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i == 100)
+		{
+			printf("Buzz");
+			break;
+		} else if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz");
+			printf(" ");
 		} else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz");
-		} else if (i % 3 == 0 && i % 5 != 0)
+			printf(" ");
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
-		} else if (i == 1)
-		{
-			printf("%d",  i);
+			printf(" ");
 		} else
 		{
-			printf(" %d", i);
+			printf("%d", i);
+			printf(" ");
 		}
 	}
 	printf("\n");
